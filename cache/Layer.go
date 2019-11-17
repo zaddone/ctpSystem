25f1e25c0e6d250214ff1a427952a5f0e0fc850d
zaddone <zaddone@qq.com>
2019-11-17 10:15:52 +0800

    up

--- cache/Layer.go
+++ cache/Layer.go
@@ -429,11 +429,12 @@ func (self *Layer) add(c Element) bool {
 		}
 	}
 
-	if (splitID == 0) &&
-	(self.tag == 1) &&
+	//if (splitID == 0) &&
+	if (self.tag == 1) &&
 	(self.par != nil) &&
+	(self.tem == nil) &&
 	//(len(self.ca.Orders) == 0) &&
-	self.par.CheckCansLong(self.direction<0) &&
+	self.par.CheckCansLong(self.direction>0) &&
 	(math.Abs(self.direction) > self.par.GetAmplitude(self.direction>0)) {
 		self.getTemplate(self.direction<0)
 	}
